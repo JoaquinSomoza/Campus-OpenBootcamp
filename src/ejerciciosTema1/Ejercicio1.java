4d7e07fd1426dd49b9c4fca22a42dec02c2a8d84
JoaquinSomoza <joaquinsomoza6@gmail.com>
2022-06-21 16:17:40 -0300

    Resolucion del ejercicio tema 1

---
+++ src/ejerciciosTema1/Ejercicio1.java
@@ -0,0 +1,31 @@
+package ejerciciosTema1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        //numeros
+        float variable1 = 1.1f;
+        double variable2 = 1.2d;
+        byte variable3 = 1;
+        short variable4 = 22;
+        int variable5 = 3;
+        long variable6 = 11111;
+
+        //textos
+        char variable7 = 'a';
+        String variable8 = "texto mas largo";
+        //boolean
+        boolean variable9= true;
+        boolean variable10= false;
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(variable7);
+        System.out.println(variable8);
+        System.out.println(variable9);
+        System.out.println(variable10);
+    }
+}
